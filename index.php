8c55aa31caae83cb0b5d5896cd8508b2dc2981b9
cesar arroyo <cesararroyo1234@gmail.com>
2015-04-29 12:02:58 -0700

    test

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 <html>
 <head>
 	<title>Quiz 2</title>
-<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="javascript" href="js/main.js">
 </head>
 <body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+<div class="dan">
+	<input type="button" name="button" placeholder="Add new item..."/>
+</div>
+
+
+<div id="iwao">
+	<input type="button" name="new-task" placeholder="Add new item..."/>
+</div>
+
+
 
 </body>
 </html>
---
+++ main.js
@@ -0,0 +1,4 @@
+// Event setup using a convenience method
+$( "dan" ).click(function() {
+    console.log( "You clicked a paragraph!" );
+});
