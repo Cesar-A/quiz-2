df3bace946435330e5d6b83bae52a39f2255a49d
cesar arroyo <cesararroyo1234@gmail.com>
2015-04-29 12:06:48 -0700

    done

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 <html>
 <head>
 	<title>Quiz 2</title>
-<link rel="stylesheet" type="javascript" href="js/main.js">
+<link rel="stylesheet" type="text/javascript" href="js/main.js">
 </head>
 <body>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 
 <div class="dan">
-	<input type="button" name="button" placeholder="Add new item..."/>
+<button>click</button>
 </div>
 
 
 <div id="iwao">
-	<input type="button" name="new-task" placeholder="Add new item..."/>
+	<button>click</button>
 </div>
 
 
